b10c5c2db9c65a98add01650a45dd01370fc673d
Tzuzaki <85707823+Tzuzaki@users.noreply.github.com>
2021-06-15 15:54:36 -0400

    a

--- ex10.py
+++ ex10.py
@@ -13,3 +13,25 @@ print (tabby_cat)
 print (persian_cat)
 print (backslash_cat)
 print (fat_cat)
+
+
+"""Escape What it does.
+\\ Backslash (\)
+\" Double- quote (")
+\a ASCII bell (BEL)
+\b ASCII backspace (BS)
+\f ASCII formfeed (FF)
+\n ASCII linefeed (LF)
+\N{name} Character named name in the Unicode database (Unicode only)
+\r ASCII carriage return (CR)
+\t ASCII horizontal tab (TAB)
+\uxxxx Character with 16- bit hex value xxxx (Unicode only)
+\Uxxxxxxxx Character with 32- bit hex value xxxxxxxx (Unicode only)
+\v ASCII vertical tab (VT)
+\ooo Character with octal value oo
+\xhh Character with hex value hh 
+"""
+
+while True:
+    for i in ["/","- ","|","\\","|"]:
+        print ("%s\r" % i,)
